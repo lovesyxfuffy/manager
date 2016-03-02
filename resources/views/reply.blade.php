b062c4bc73d683f7b3bbeaba3cc00bb2f41c6954
lovesyxfuffy <lovesyx21>
2016-03-02 21:12:43 +0800

    user model

---
+++ resources/views/reply.blade.php
@@ -0,0 +1,322 @@
+@extends('blank')
+@section('title')
+    论坛
+@stop
+
+@section('head')
+    <link rel="stylesheet" href="https://sf-static.b0.upaiyun.com/v-56d41406/global/css/global.css">
+    <link type="text/css" rel="stylesheet" href="chrome-extension://kfgmnlgjmofpiicpgohgfpeabgpmhjdp/style.css">
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-xs-12 col-md-9 main">
+            <article class="widget-question__item">
+                <div class="post-col">
+                    <div class="widget-vote">
+                        <button type="button" class="like" data-id="1010000004527231" data-type="question" data-do="like" data-toggle="tooltip" data-placement="top" title="" data-original-title="问题对人有帮助，内容完整，我也想知道答案">
+                            <span class="sr-only">问题对人有帮助，内容完整，我也想知道答案</span>
+                        </button>
+                        <span class="count">0</span>
+                        <button type="button" class="hate" data-id="1010000004527231" data-type="question" data-do="hate" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="问题没有实际价值，缺少关键内容，没有改进余地">
+                            <span class="sr-only">问题没有实际价值，缺少关键内容，没有改进余地</span>
+                        </button>
+                    </div>
+                    <!-- end .widget-vote -->
+                </div>
+
+                <div class="post-offset">
+                    <div class="question fmt">
+                                                            <pre class="hljs"><div class="widget-codetool" style="display: none;"><button class="selectCode btn btn-xs">全选</button><button href="javascript:void(0);" class="copyCode btn btn-xs" data-clipboard-text="移动web页面（浏览器打开的web应用，非App内置的Web页面）内有分享按钮，点击可分享到朋友圈，微信好友，QQ......
+据说是使用相关插件，求指导！
+" data-toggle="tooltip" data-placement="top" title="">复制</button><button href="javascript:void(0);" class="saveToNote btn btn-xs">放进笔记</button></div><code>移动web页面（浏览器打开的web应用，非App内置的Web页面）内有分享按钮，点击可分享到朋友圈，微信好友，QQ......
+                                                                    据说是使用相关插件，求指导！
+                                                                </code></pre>
+
+                    </div>
+
+
+
+                    <div class="row">
+                        <div class="post-opt col-md-8">
+                            <ul class="list-inline mb0">
+                                <li><a href="/q/1010000004527231">6 分钟前提问</a></li>
+                                <li data-toggle="tooltip" data-placement="top" title="" class="edit-btn" data-original-title="声望达到250开启">
+                                    编辑
+                                </li>
+
+                                <li><a href="javascript:void(0);" class="comments" data-id="1010000004527231" data-target="#comment-1010000004527231">
+                                        评论</a></li>
+
+
+
+
+
+
+                                <li class="dropdown">
+                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">更多<b class="caret"></b></a>
+                                    <ul class="dropdown-menu dropdown-menu-left">
+                                        <li><a href="#911" data-id="1010000004527231" data-toggle="modal" data-target="#911" data-type="question" data-typetext="问题">举报</a>
+                                        </li>
+
+
+
+
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+
+
+                    </div>
+                    <div class="widget-comments hidden" id="comment-1010000004527231" data-id="1010000004527231">
+                        <div class="widget-comments__form row">
+
+                            <form class="clearfix" method="POST">
+                                <div class="form-group mb0 widget-comments__form--input">
+                                    <input name="id" type="hidden" value="1010000004527231">
+                                    <textarea rows="1" name="text" class="form-control mono" id="commentText-1010000004527231" data-id="1010000004527231" placeholder="使用评论询问更多信息或提出修改意见，请不要在评论里回答问题"></textarea>
+
+                                </div>
+                                <div class="widget-comments__btn">
+                                    <div class="pull-right">
+                                        <button type="submit" class="btn btn-default postComment" data-id="1010000004527231">提交评论
+                                        </button>
+                                <span class="widget-comments__btn--tips"><a href="javascript:void(0);" class="toggle-comment-helper" title="语法提示">
+                                        <i class="fa fa-question-circle"></i>
+                                    </a></span>
+                                    </div>
+                                </div>
+                            </form>
+                            <div class="col-md-12">
+                                <div class="alert alert-warning alert-dismissible mb0 mt10 fmt comment-helper" data-rank="0" style="display:none;" role="alert">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="right:0;"><span aria-hidden="true">×</span></button>
+                                    评论支持部分 Markdown 语法：<code>**bold**</code> <code>_italic_</code>
+                                    <code>[link](http://example.com)</code> <code>&gt; 引用</code> <code>`code`</code> <code>-
+                                        列表</code>。<br>同时，被你 @ 的用户也会收到通知
+                                </div>
+                            </div>
+
+
+                        </div><!-- /.widget-comments__form -->
+                    </div><!-- /.widget-comments -->
+
+
+                </div>
+                <!-- end .post-offset -->
+            </article>
+
+            <div class="widget-answers">
+                <div class="btn-group pull-right" role="group">
+                    <a href="/q/1010000004527231#answers-title" id="sortby-rank" class="btn btn-default btn-xs active">默认排序</a>
+                    <a href="?sort=created#answers-title" id="sortby-created" class="btn btn-default btn-xs">时间排序</a>
+                </div>
+
+                <h2 class="title h4 mt30 mb20 post-title" id="answers-title">1 个回答</h2>
+
+
+
+
+
+                <article class="clearfix widget-answers__item" id="a-1020000004527247">
+                    <div class="post-col">
+                        <div class="widget-vote">
+                            <button type="button" class="like" data-id="1020000004527247" data-type="answer" data-do="like" data-toggle="tooltip" data-placement="top" title="" data-original-title="答案对人有帮助，有参考价值">
+                                <span class="sr-only">答案对人有帮助，有参考价值</span>
+                            </button>
+                            <span class="count">0</span>
+                            <button type="button" class="hate" data-id="1020000004527247" data-type="answer" data-do="hate" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="答案没帮助，是错误的答案，答非所问">
+                                <span class="sr-only">答案没帮助，是错误的答案，答非所问</span>
+                            </button>
+
+                        </div>
+                    </div>
+
+                    <div class="post-offset">
+                        <div class="answer fmt">
+                            <p>如果不是app内置web页面,只能使用js的一键分享.具体详情可以百度<code>一键分享</code>,有许多分享平台.<br>如果是app的话还可以使用混淆交互,具体详情百度<code>js android 交互</code></p>
+                        </div>
+
+
+
+                        <div class="row answer__info--row">
+                            <div class="post-opt col-md-8 col-sm-8 col-xs-10">
+                                <ul class="list-inline mb0">
+
+                                    <li><a href="/q/1010000004527231/a-1020000004527247"> 1 分钟前回答</a> <span class="text-muted">
+
+                    </span></li>
+                                    <li data-toggle="tooltip" data-placement="top" title="" class="edit-btn" data-original-title="声望达到500开启">编辑</li>
+
+                                    <li><a href="javascript:void(0);" class="comments" data-id="1020000004527247" data-target="#comment-1020000004527247">
+                                            评论</a></li>
+
+                                    <li class="dropdown">
+                                        <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">更多<b class="caret"></b></a>
+                                        <ul class="dropdown-menu dropdown-menu-left">
+                                            <li><a href="#911" data-id="1020000004527247" data-toggle="modal" data-target="#911" data-type="answer" data-typetext="答案">举报</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="col-md-2 col-sm-2 col-xs-2 answer__info--author-avatar">
+                                <a class="mr10" href="/u/august1996"><img class="avatar-32" src="https://sfault-avatar.b0.upaiyun.com/379/989/3799891567-565fa729f1bc6_big64" alt=""></a>
+                            </div>
+                            <div class="col-md-2 col-sm-2 hidden-xs answer__info--author">
+                                <div class=" answer__info--author-warp">
+                                    <a class="answer__info--author-name" title="august1996" href="/u/august1996">august1996</a><span class="answer__info--author-rank">18 声望</span></div>
+                            </div>
+                        </div>
+
+
+                        <div class="widget-comments hidden" id="comment-1020000004527247" data-id="1020000004527247">
+                            <div class="widget-comments__form row">
+
+                                <form class="clearfix" method="POST">
+                                    <div class="form-group mb0 widget-comments__form--input">
+                                        <input name="id" type="hidden" value="1020000004527247">
+                                        <textarea rows="1" name="text" class="form-control mono" id="commentText-1020000004527247" data-id="1020000004527247" placeholder="使用评论询问更多信息或提出修改意见，请不要在评论里回答问题"></textarea>
+
+                                    </div>
+                                    <div class="widget-comments__btn">
+                                        <div class="pull-right">
+                                            <button type="submit" class="btn btn-default postComment" data-id="1020000004527247">提交评论
+                                            </button>
+                                <span class="widget-comments__btn--tips"><a href="javascript:void(0);" class="toggle-comment-helper" title="语法提示">
+                                        <i class="fa fa-question-circle"></i>
+                                    </a></span>
+                                        </div>
+                                    </div>
+                                </form>
+                                <div class="col-md-12">
+                                    <div class="alert alert-warning alert-dismissible mb0 mt10 fmt comment-helper" data-rank="0" style="display:none;" role="alert">
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="right:0;"><span aria-hidden="true">×</span></button>
+                                        评论支持部分 Markdown 语法：<code>**bold**</code> <code>_italic_</code>
+                                        <code>[link](http://example.com)</code> <code>&gt; 引用</code> <code>`code`</code> <code>-
+                                            列表</code>。<br>同时，被你 @ 的用户也会收到通知
+                                    </div>
+                                </div>
+
+
+                            </div><!-- /.widget-comments__form -->
+                        </div><!-- /.widget-comments -->
+
+
+
+
+                    </div>
+                </article><!-- /article -->
+
+
+
+
+                <div class="text-center">
+
+                </div>
+            </div><!-- /.widget-answers -->
+
+
+
+            <div style="padding-left: 65px;">
+                <h4>撰写答案</h4>
+                <input type="hidden" id="draftId" value="">
+                <input type="hidden" value="1010000004527231" id="questionId">
+                <form action="/question/1010000004527231/answers/add" method="post" class="editor-wrap">
+                    <div class="editor" id="questionText">
+                        <textarea id="answerEditor" name="text" class="form-control" rows="4" placeholder="撰写答案..."></textarea>
+                    </div>
+                    <div id="answerSubmit" class="hide mt15 clearfix">
+                        <div class="checkbox pull-left">
+                            <label><input type="checkbox" class="" id="shareToWeibo">
+                                同步到新浪微博</label>
+                        </div>
+                        <div class="pull-right">
+                    <span id="editorStatus" class="hidden text-muted">
+
+                    </span>
+                            <a id="dropIt" href="javascript:void(0);" class="mr10 hidden">[舍弃]</a>
+                            <button type="submit" id="answerIt" data-id="1010000004527231" class="btn btn-lg btn-primary ml20">提交回答
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div class="widget-invite mt30">
+                <p>
+                    <span class="text-muted">不知道答案？你还可以</span>
+                    <a href="#" class="btn btn-default btn-sm inviteBtn ml5" data-title="移动web页面如何实现分享到微信、QQ等分享功能？" data-url="http://sfau.lt/bNs9T1" data-id="1010000004527231">邀请回答</a>
+                </p>
+            </div>
+
+        </div>
+        <!-- /.main -->
+
+
+
+        <div class="col-xs-12 col-md-3 side">
+
+            <div class="widget-box no-border">
+                <h2 class="h4 widget-box__title">相似问题</h2>
+                <ul class="widget-links list-unstyled">
+                    <li class="widget-links__item"><a title="javascript移动端web可以左右移动的导航栏应该如何布局" href="/q/1010000003800345">javascript移动端web可以左右移动的导航栏应该如何布局</a>
+                        <small class="text-muted">
+                            2 回答
+                        </small>
+                    </li>
+                    <li class="widget-links__item"><a title="javascript 后台执行代码" href="/q/1010000002876281">javascript 后台执行代码</a>
+                        <small class="text-muted">
+                            1 回答
+                        </small>
+                    </li>
+                    <li class="widget-links__item"><a title="Web应用集成IM功能" href="/q/1010000002619606">Web应用集成IM功能</a>
+                    </li>
+                    <li class="widget-links__item"><a title="如何组织前端javascript代码" href="/q/1010000003830319">如何组织前端javascript代码</a>
+                        <small class="text-muted">
+                            5 回答
+                            |                             已解决
+                        </small>
+                    </li>
+                    <li class="widget-links__item"><a title="关于parse-server，javascript-SDK在WEB环境下开发的安全性问题" href="/q/1010000004415998">关于parse-server，javascript-SDK在WEB环境下开发的安全性问题</a>
+                        <small class="text-muted">
+                            1 回答
+                        </small>
+                    </li>
+                    <li class="widget-links__item"><a title="用chrome调试移动web页面，不触发tap事件" href="/q/1010000003927435">用chrome调试移动web页面，不触发tap事件</a>
+                        <small class="text-muted">
+                            2 回答
+                            |                             已解决
+                        </small>
+                    </li>
+                    <li class="widget-links__item"><a title="javascript 实现复制链接" href="/q/1010000003064003">javascript 实现复制链接</a>
+                        <small class="text-muted">
+                            2 回答
+                        </small>
+                    </li>
+                    <li class="widget-links__item"><a title="能否使用JavaScript来ping一个IP？" href="/q/1010000002735229">能否使用JavaScript来ping一个IP？</a>
+                        <small class="text-muted">
+                            3 回答
+                        </small>
+                    </li>
+                    <li class="widget-links__item"><a title="javascript实现startswith" href="/q/1010000002569299">javascript实现startswith</a>
+                        <small class="text-muted">
+                            4 回答
+                            |                             已解决
+                        </small>
+                    </li>
+                    <li class="widget-links__item"><a title="web QQ 前端桌面效果是如何实现的" href="/q/1010000000200630">web QQ 前端桌面效果是如何实现的</a>
+                        <small class="text-muted">
+                            2 回答
+                            |                             已解决
+                        </small>
+                    </li>
+                </ul>
+            </div>
+            <div class="widget-share__full" data-text="移动web页面如何实现分享到微信、QQ等分享功能？" data-url="https://segmentfault.com/q/1010000004527231" data-shorturl="http://sfau.lt/bNs9T1"><div style="margin-bottom:10px;">分享扩散：</div><div class="widget-share-network sharer-0" style="display: block;"><ul class="sn-inline"><li data-network="weibo"><a href="javascript:void(0);" class="entypo-weibo icon-sn-weibo share-1" data-toggle="tooltip" data-placement="top" title="" data-original-title="分享至新浪微博">新浪微博</a></li><li data-network="wechart"><a href="javascript:void(0);" class="entypo-wechart icon-sn-weixin share-2" data-toggle="tooltip" data-placement="top" title="" data-original-title="分享至微信">微信</a></li><li data-network="twitter"><a href="javascript:void(0);" class="entypo-twitter icon-sn-twitter share-3" data-toggle="tooltip" data-placement="top" title="" data-original-title="分享至 Twitter">Twitter</a></li><li data-network="facebook"><a href="javascript:void(0);" class="entypo-facebook icon-sn-facebook share-4" data-toggle="tooltip" data-placement="top" title="" data-original-title="分享至 Facebook">Facebook</a></li></ul></div><a type="button" class="btn btn-sm shareMore btn-sn-more" data-toggle="popover" data-placement="top" data-url="https://segmentfault.com/q/1010000004527231" data-shorturl="http://sfau.lt/bNs9T1"><span class="icon-sn-dotted" data-original-title="" title=""></span><strong class="visible-xs-inline">•••</strong></a></div>
+
+
+        </div><!-- /.side -->
+
+    </div>
+@stop
