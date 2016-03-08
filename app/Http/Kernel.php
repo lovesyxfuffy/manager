bcbf0c2d466786169b9eb8ff4487dcb5ccdcb345
lovesyxfuffy <lovesyx21>
2016-03-08 23:35:55 +0800

    change kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
         ],
 
-        'web-csrf' => [
+        'web-no-csrf' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
