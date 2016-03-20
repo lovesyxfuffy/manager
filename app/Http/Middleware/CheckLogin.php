8d73dc1e241aa21cfd215c410c54d920a07b0e7c
lovesyxfuffy <lovesyx21>
2016-03-20 15:47:25 +0800

    CheckLogin中间件 session加入username

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -19,8 +19,9 @@ class CheckLogin{
             if($username!=''&&$password!=''){
                 $result = DB::table('user')->where('username', $username)->where('password', $password);
                 if ($result->count('id')==1) {
-                    $id = $result->first(['id'])->id;
-                    session(['user_id' => $id]);
+                    $user = $result->first(['id','username']);
+                    session(['user_id' => $user->id]);
+                    session(['username'=>$user->username]);
                     return $next($request);
                 } else
 
